879b37db23890091f3282bfce1569d6c14f05251
“ayush1120” <ayushsharma1161@gmail.com>
2020-04-06 01:50:10 +0530

    Fixed setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,13 @@ BASE_DIR="${PROJECT_DIR}/scarf"
 
 
 command cd "$PROJECT_DIR"
+DIR="envCRDT-DisCS"
+if [ -d "$DIR" ]; then
+	## Take action if $DIR exists ###
+  echo "${DIR} exists. Removing current environment."
+	command rm -rf ${DIR}
+fi
+echo "Creating New Environment"
 command virtualenv -p "${PYTHONPATH}" envCRDT-DisCS
 command source "${PROJECT_DIR}/envCRDT-DisCS/bin/activate"
 command pip install -r "${PROJECT_DIR}/requirements.txt"
-command deactivate
